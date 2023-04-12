6656603348fcd77fbcdb26277f4c10b63d3dbfe9
solmazkadir <abdlkadirsolmaz@gmail.com>
2023-04-12 22:11:16 +0300

    admin products ve contacts sayfaları

---
+++ P013WebSite/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P013WebSite.Data;
+using P013WebSite.Entities;
+using P013WebSite.Tools;
+
+namespace P013WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public SlidersController(DatabaseContext context)
+        {
+            _context = context; //S.O.L.I.D PRENSİPLERİ
+        }
+
+        // GET: SlidersController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _context.Sliders.ToListAsync();
+            return View(model);
+        }
+
+        // GET: SlidersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SlidersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SlidersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Slider collection,IFormFile? Image)
+        {
+            try
+            {
+                collection.Image = await FileHelper.FileLoaderAsync(Image);
+                await _context.Sliders.AddAsync(collection);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidersController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var model =await _context.Sliders.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: SlidersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, Slider collection, IFormFile? Image)
+        {
+            try
+            {
+                if (Image is not null)
+                collection.Image = await FileHelper.FileLoaderAsync(Image);
+                _context.Sliders.Update(collection); //entity frameworkde update ve delete metotları asenkron çalışmıyor add ve listeleme metotları async 
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidersController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _context.Sliders.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: SlidersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, Slider collection)
+        {
+            try
+            {
+                _context.Sliders.Remove(collection);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
